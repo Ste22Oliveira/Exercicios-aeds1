ca6c43e4bb6c199087876090dbb2f5f7d8db61dd
Ste22Oliveira <114537431+Ste22Oliveira@users.noreply.github.com>
2022-09-27 20:38:33 -0300

    Numerospares

---
+++ Númerospares.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 0044725
+ */
+public class Númerospares {
+     public static void main(String[] args) {
+        for(int i=0; i<=100 ; i++){
+        if(i%2==0){
+            System.out.println(i);
+        }
+     }
+}
+}
