45f56045d91686e314668aa1416c2ffbe3779d17
Ste22Oliveira <114537431+Ste22Oliveira@users.noreply.github.com>
2022-10-03 21:50:36 -0300

    Metodo

---
+++ Metodo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aeds;
+
+/**
+ *
+ * @author 0044725
+ */
+public class Metodo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println(soma(20,40));
+       
+      
+    }
+    public static int soma(int a, int b){
+        return a +b;
+    }
+    
+    
+}
